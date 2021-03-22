6936a6c9039b1d448179a59d99c0fe9978b5b59a
mizidoro <matheusizidoro07@gmail.com>
2021-03-22 19:35:08 -0300

    adds brute force method

--- closest.cpp
+++ closest.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>  
 #include <string>
 #include <sstream>
- 
+#include <math.h>
+#include <time.h>
+
 using namespace std;
 
-double** ObterPares(char* nome_do_arquivo);
+double** ObterPares(char* nome_do_arquivo, int* num_pontos);
+
+void ResolverForcaBruta(double** pares, int num_pontos);
 
 int main(int argc, char* argv[])    
 {    
-    if(argv[0])
-    double** pares = ObterPares(argv[1]);  
+    if(!argv[0])
+    {
+        cout << "Eh preciso informar um arquivo de entrada" << endl;
+        return -1;
+    }
+    int *num_pontos;
+    double** pares = ObterPares(argv[1], num_pontos);  
+    ResolverForcaBruta(pares, *num_pontos);
     return 0;    
 }
 
-double** ObterPares(char* nome_do_arquivo)
+double** ObterPares(char* nome_do_arquivo, int* num_pontos)
 {
     ifstream arquivo;
     double** pares;
@@ -23,11 +34,12 @@ double** ObterPares(char* nome_do_arquivo)
     arquivo.open(nome_do_arquivo);
     if(!arquivo)
     {
-        cout << "Arquivo nao encontrado!";
+        cout << "Arquivo nao encontrado!" << endl;
     }
     else
     {
         getline(arquivo, line);
+        *num_pontos = stoi(line);
         int count = stoi(line);
         pares = (double**)malloc(count*sizeof(double*));
         int i = 0;
@@ -50,4 +62,36 @@ double** ObterPares(char* nome_do_arquivo)
     return pares;
 }
 
+void ResolverForcaBruta(double** pares, int num_pontos)
+{
+    double *par1, *par2;
+    double distancia = 4297967296;
+    clock_t t;
+    
+    t = clock();
 
+    for(int i=0; i<num_pontos; i++)
+    {
+        for(int j=0; j<num_pontos; j++)
+        {
+            if(i==j) continue;
+
+            double dx = pares[i][0]-pares[j][0];
+            double dy = pares[i][1]-pares[j][1];
+
+            double dist = sqrt(pow(dx,2) + pow(dy,2));
+
+            if(dist < distancia)
+            {
+                distancia = dist;
+                par1 = pares[i];
+                par2 = pares[j];
+            }
+        }
+    }
+
+    t = clock() - t;
+
+    cout << fixed << setprecision(6);
+    cout << t*1.0/CLOCKS_PER_SEC << ' ' << distancia << ' ' << par1[0] << ' ' << par1[1] << ' ' << par2[0] << ' ' << par2[1];
+}
